fc53bf28b48110730e807d74045332ed5f0ec8a7
HDuarteJ <132863291+HDuarteJ@users.noreply.github.com>
2025-09-09 17:22:32 +0000

    created cep146 folder

---
+++ cep146/hello.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main(void)
+{
+    printf("Hello, World\n");
+}
+
+/* 
+
+1- Preprocess
+2- compiles
+3- optimized
+4- Assemble
+5- Link
+
+Ther assembling stage producess the file a.out, unless the developer name the file on gcc execution.
+
+
+gcc -o compiles de code in a optimized mode
+
+
+
+*/

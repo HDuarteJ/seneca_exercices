5c508b1181c18cdff4d56d16334118d5f94233d2
HDuarteJ <132863291+HDuarteJ@users.noreply.github.com>
2025-09-15 16:57:12 +0000

    Added functions exercices - ipc144

---
+++ python/test.py
@@ -0,0 +1,7 @@
+x, y = 1, 2
+
+def func(x,y):
+    y = y**2
+    z = x + y
+    return z
+
---
+++ python/two_sum.py
@@ -0,0 +1,8 @@
+class Solution:
+    def twoSum(lista, target):
+        hash_map = {1:2}
+        print(hash_map.values())
+        return 0
+    
+Solution.twoSum([2,7,11,15], 9)
+
